516d7a7ef722ee58be4f4ba8450cdd97b251ea16
criocreatives <y898nht49m@privaterelay.appleid.com>
2026-09-25 13:32:07 -0500

    Remove obsolete new-heights preview path

--- new-heights/script.js
+++
@@ -1,1 +0,0 @@
-const m=document.querySelector('.menu'),n=document.querySelector('nav');m.onclick=()=>n.classList.toggle('open');document.querySelectorAll('nav a').forEach(a=>a.onclick=()=>n.classList.remove('open'));document.getElementById('form').onsubmit=e=>{e.preventDefault();document.getElementById('note').style.display='block'};
